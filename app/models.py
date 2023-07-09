c63be0ca511aa0c79bee784cd21145e1b1c14808
monja119 <monja.sesame@gmail.com>
2023-07-09 19:39:50 +0300

    updating

--- app/models.py
+++ app/models.py
@@ -1,3 +1,44 @@
+import os
 from django.db import models
 
-# Create your models here.
+
+def get_upload_path(instance, filename):
+    # getting the file instance type
+    media_type = instance.media_type
+
+    # setting the upload path according to the file type
+    if media_type == 'image':
+        upload_path = 'images'
+    elif media_type == 'video':
+        upload_path = 'videos'
+    elif media_type == 'audio':
+        upload_path = 'videos'
+    else:
+        upload_path = 'others'
+
+    return os.path.join(upload_path, filename)
+
+
+media_type_choices = (
+    ['image', 'image'],
+    ['video', 'video'],
+    ['audio', 'audio'],
+)
+
+
+class Media(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    media_type = models.CharField(max_length=10, default='image', choices=media_type_choices)  # photo, video, mp3
+    link = models.FileField(upload_to=get_upload_path)
+
+    def save(self, *args, **kwargs):
+        return super(Media, self).save(*args, **kwargs)
+
+
+class Produits(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()   # en Ar
+    date = models.DateTimeField(auto_now=True)
+
+    media = models.ForeignKey(Media, on_delete=models.CASCADE)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import request, HttpResponse, FileResponse
 # Create your views here.
+
+
+class Home:
+    def index(self, request):
+        msg = 'ok, 5/5'
+        return render(request, 'home/index.html', locals())
+
+
+class Media:
+    def getFile(self, request, type, name):
+
+        return FileResponse(filename='../media/'+type+'/'+name, as_attachment=True)
