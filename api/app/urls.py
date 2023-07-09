6d0bc72fe072079c6f239e57454646e004c3fd52
monja119 <monja.sesame@gmail.com>
2023-07-09 20:02:02 +0300

    separation de front et back

---
+++ api/app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from app.views import *
+
+urlpatterns = [
+    path('', Home().index, name='home'),
+    path('media/<str:type>/<str:name>', Media().getFile, name='media'),
+]
