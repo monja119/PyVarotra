d78fa1206910ec70dd2ba1361acc95f3acb11c12
monja119 <monja.sesame@gmail.com>
2023-07-09 13:37:37 +0300

    mise à jour

--- pyVarotra/settings.py
+++ pyVarotra/settings.py
@@ -37,11 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clients',
-    'articles',
-    'paniers',
-    'transactions',
-    'commentaires',
+    'app',
 ]
 
 MIDDLEWARE = [
