6d8d3db793a2f843927357e22b4b4f124743ecb3
RABETO Mac Thérèsia Modestine <115572159+Theresia-aps22@users.noreply.github.com>
2023-07-10 13:36:22 +0300

    delete

--- commentaires/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from articles.models import Article
-
-class Commentaire(models.Model):
-    id_commentaire = models.AutoField(primary_key=True)
-    id_article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    contenu = models.CharField(max_length=255)
-
