bb6994292e8bc925db79e05598c69ed6aa9ceafa
RABETO Mac Thérèsia Modestine <115572159+Theresia-aps22@users.noreply.github.com>
2023-07-10 13:34:09 +0300

    delete

--- articles/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Article(models.Model):
-    id_article = models.AutoField(primary_key=True)
-    nom_article = models.CharField(max_length=255)
-    section_article = models.CharField(max_length=255)
-    prix = models.CharField(max_length=255)
