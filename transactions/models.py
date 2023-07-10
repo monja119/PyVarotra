b504fc62db34e8ed17fc1cb5d01b3904a908c385
RABETO Mac Thérèsia Modestine <115572159+Theresia-aps22@users.noreply.github.com>
2023-07-10 13:37:04 +0300

    delete

--- transactions/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Transaction(models.Model):
-    id_transaction = models.AutoField(primary_key=True)
-    num_carte_banq = models.CharField(max_length=255)
-    montant = models.IntegerField()
-    code_autorisation = models.CharField(max_length=255)
