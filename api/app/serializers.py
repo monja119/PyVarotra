10077517b00e531005dc642030e882bf3893279c
monja119 <monja.sesame@gmail.com>
2023-07-17 11:20:19 +0300

    categories

--- api/app/serializers.py
+++ api/app/serializers.py
@@ -15,7 +15,7 @@ class CategoriesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Categories
-        fields = '__all__'
+        fields = ['id', 'name']
 
 
 class ProduitsSerializer(serializers.ModelSerializer):
